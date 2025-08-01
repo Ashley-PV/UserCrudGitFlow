358c2cc3e6f4b7e94f507bb8a165ba51a4c8e8a8
ashley perez <20211573@itla.edu.do>
2025-08-01 18:44:05 -0400

    Initial commit: Proyecto CRUD básico funcionando

---
+++ UserCRUD/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UserCRUD.Models;
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserCRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
